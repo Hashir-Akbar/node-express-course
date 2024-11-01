43ef20a868e66e678646ea2b50327c62b61ad608
Hashir Akbar <hashirbusinessinfo@gmail.com>
2024-11-01 23:37:29 +0500

    working on task manager backend

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,1 +1,13 @@
-console.log('Task Manager App')
+import express from "express";
+
+const app = express();
+
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send("tast manager");
+});
+
+app.listen(PORT, () => {
+  console.log("your app is running on http://localhost:" + PORT);
+});
