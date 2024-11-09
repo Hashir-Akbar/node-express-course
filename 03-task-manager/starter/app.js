6afbb1f9a4d8e6c409ec8457fdda65bad98a9576
Hashir Akbar <hashirbusinessinfo@gmail.com>
2024-11-09 23:49:59 +0500

    working on rest api

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -11,3 +11,12 @@ app.get("/", (req, res) => {
 app.listen(PORT, () => {
   console.log("your app is running on http://localhost:" + PORT);
 });
+
+// Rest Api Design
+ 
+//  app.get("api/v1/tasks")         - get all tasks
+//  app.post("api/v1/tasks")        - create new tasks
+//  app.get("api/v1/tasks/:id")     - get single tasks
+//  app.patch("api/v1/tasks/:id")   - update tasks
+//  app.delete("api/v1/tasks/:id")  - delete tasks
+
